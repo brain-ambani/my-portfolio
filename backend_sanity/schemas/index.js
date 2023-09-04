0a1ca7ac9cf5211fa34b871d3d143e2945f0a303
brian-ambani <brainambani1@gmail.com>
2023-09-04 16:51:37 +0300

    updated the backend with sanity

---
+++ backend_sanity/schemas/index.js
@@ -0,0 +1,19 @@
+import testimonials from "./testimonials";
+import abouts from "./abouts";
+import brands from "./brands";
+import skills from "./skills";
+import experiences from "./experiences";
+import workExperience from "./workExperience";
+import works from "./works";
+import contact from "./contact";
+
+export const schemaTypes = [
+    testimonials,
+    abouts,
+    brands,
+    skills,
+    experiences,
+    workExperience,
+    works,
+    contact
+]
